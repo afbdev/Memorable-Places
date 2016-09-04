a545f83de8dcdd3ffe84ca63688ca93595aabd6e
afbdev <afbdev@alexanders-mbp>
2016-09-04 14:03:38 -0500

    Default Place, MapVC Connected

--- Memorable Places/ViewController.swift
+++ Memorable Places/ViewController.swift
@@ -7,12 +7,51 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(activePlace)
+        if activePlace != -1 {
+            
+            // Get place details to display on map
+            
+            print(places[activePlace])
+            
+            
+            if places.count > activePlace {
+                if let name = places[activePlace]["name"] {
+                    if let lat = places[activePlace]["lat"] {
+                        if let lon = places[activePlace]["lon"] {
+                            if let latitude = Double(lat) {
+                                if let longitude = Double(lon) {
+                                    
+                                    let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                                    let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                                    let region = MKCoordinateRegion(center: coordinate, span: span)
+                                    self.map.setRegion(region, animated: true)
+                                    
+                                    let annotation = MKPointAnnotation()
+                                    annotation.coordinate = coordinate
+                                    annotation.title = name
+                                    self.map.addAnnotation(annotation)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
